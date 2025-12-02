33755fd716931e6269f07430bbee3730c8d20b96
alonso.rogpas <alonso.rogpas@educa.jcyl.es>
2025-12-02 12:12:30 +0100

    Se añade una nueva rama

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.5") // Navegacion
 
     //---------------------------------
-    implementation("androidx.compose.material:material-icons-extended:1.5.0-alpha08") // Para los iconos
+    implementation("androidx.compose.material:material-icons-extended:1.5.0-alpha08") // Para los iconosb
     implementation("androidx.compose.material3:material3:1.5.0-alpha08") // Para tener el material3 lo más actualizado de jetpack compose
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
